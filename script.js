24c1fdf0b3d38c197195d18297d3687b517fc189
Amira Elkhateeb <elkh0054@algonquinlive.com>
2021-11-11 19:43:39 -0500

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+const puppies = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg']
+const items = []
+const $container = document.getElementById('gallery')
+for (const puppy of puppies) {
+    items.push(`<header><span>&#${gallery}</span> <span>${gallery}</span></div>`)
+}
+
+$container.innerHTML = items.join('')
