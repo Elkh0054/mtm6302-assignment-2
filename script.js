d649c00eff7e8fcbc4f00e0be1fce01fff61a23e
Amira Elkhateeb <elkh0054@algonquinlive.com>
2021-11-11 21:23:36 -0500

    update

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const items = []
 // adding the images//
 
 
-
 const $gallery = document.getElementById('gallery')
 for (let i = 1; i < 13; i++ ) {
     items.push(`<li class='puppy'><img tabindex=${i} src = 'images/${i}.jpg'></li>`)
@@ -13,3 +12,42 @@ for (let i = 1; i < 13; i++ ) {
 }
 
 $gallery.innerHTML = items.join('')
+
+//adding lightbox to photos 
+
+const $lightbox = document.getElementById('lightbox')
+$lightbox.style.visibility = 'hidden'
+
+//adding function 
+
+function imgClicked(myImage) {
+    $lightbox.innerHTML = `<button id = 'imgClose'>close</button><img src = '${myImage}'>`
+    $lightbox.style.visibility = 'visible'
+
+ //closing the opened image
+ const $closeDiv = document.getElementById('imgClose')
+ $closeDiv.addEventListener('click', function(){
+     $lightbox.innerHTML=''
+     $lightbox.style.visibility = 'hidden'
+ })   
+}
+
+//adding eventlistener to the gallery using delegation 
+
+$gallery.addEventListener('click', function (event) {
+    const $targetItem = event.target.closest('img');
+    if($targetItem){
+        imgClicked($targetItem.src)
+    }
+})
+
+$gallery.addEventListener('mouseover', function (event) {
+let $targetItem = event.target.closest('img');
+$targetItem.classList.add('larger')
+
+})
+
+$gallery.addEventListener('mouseout', function (event){
+    let $targetItem1 =event.target.closest('img');
+    $targetItem1.classList.remove('larger')
+})
